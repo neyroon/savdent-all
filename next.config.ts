043ecec4b24dfdf2b9a15681a0e76aeb96a82de3
Alexander <aosipov@inno.tech>
2025-03-28 08:00:10 +0400

    ds

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
       {
@@ -11,7 +12,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  ignoreDuringBuilds: true,
 };
 
 export default nextConfig;
