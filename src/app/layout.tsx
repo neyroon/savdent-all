7176af59d6c022a957364cc3b31175fe7a98a408
Alexander <aosipov@inno.tech>
2025-04-01 15:33:25 +0400

    ym

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
+import { YandexMetrika } from "@/components/metrika/metrika";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -19,6 +21,24 @@ export const metadata: Metadata = {
   },
 };
 
+const yandexScript = `<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(100720922, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/100720922" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->`;
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,7 +46,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={`${roboto.variable} antialiased`}>{children}</body>
+      <body className={`${roboto.variable} antialiased`}>
+        <Suspense>
+          <YandexMetrika />
+        </Suspense>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/metrika/metrika.tsx
@@ -0,0 +1,32 @@
+import Script from "next/script";
+import React from "react";
+
+export const YandexMetrika = () => {
+  return (
+    <>
+      <Script id="yandex-metrika">{`
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(100720922, "init", {
+        defer: true,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`}</Script>
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/100720922"
+            style="position:absolute; left:-9999px;"
+            alt=""
+          />
+        </div>
+      </noscript>
+    </>
+  );
+};
